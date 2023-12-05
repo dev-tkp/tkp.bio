0066b25f5b5506f66b85db49366e9936c2b59789
dev-tkp <tkpark0504@gmail.com>
2023-12-06 07:16:31 +0900

    changing videos from .mp4 to .gif

--- src/videoPostsData.js
+++ src/videoPostsData.js
@@ -1,11 +1,21 @@
-// src/videoPostsData.js
+// videoPostsData.js
 const videoPosts = [
-    {
-      id: 1,
-      videoSrc: 'videos/demo.mp4', // Relative path from public folder
-      content: 'This is an example video post.'
-    },
-    // Add more video posts here
-  ];
-  
-  export default videoPosts;
+  {
+    id: 1,
+    videoSrc: '/videos/demo1.gif', // Update this path to your gif file
+    content: 'This is an example video post.'
+  },
+  {
+    id: 2,
+    videoSrc: '/videos/demo2.gif', // Update this path to your gif file
+    content: 'This is an example video post.'
+  },
+  {
+    id: 3,
+    videoSrc: '/videos/demo3.gif', // Update this path to your gif file
+    content: 'This is an example video post.'
+  },
+  // ... more posts
+];
+
+export default videoPosts;
