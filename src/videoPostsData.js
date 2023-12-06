3ec5978cbd4a8ab20e408b483680946f90bd8a29
dev-tkp <tkpark0504@gmail.com>
2023-12-06 21:34:06 +0900

    image troubleshooting

--- src/videoPostsData.js
+++ src/videoPostsData.js
@@ -2,17 +2,17 @@
 const videoPosts = [
   {
     id: 1,
-    videoSrc: '/videos/demo1.gif', // Update this path to your gif file
+    videoSrc: '<img src="https://dev-tkp.github.io/tkp.bio/public/demo1.gif">', // Update this path to your gif file
     content: 'This is an example video post.'
   },
   {
     id: 2,
-    videoSrc: '/videos/demo2.gif', // Update this path to your gif file
+    videoSrc: '<img src="https://dev-tkp.github.io/tkp.bio/public/demo2.gif">', // Update this path to your gif file
     content:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
   },
   {
     id: 3,
-    videoSrc: '/videos/demo3.gif', // Update this path to your gif file
+    videoSrc: '<img src="https://dev-tkp.github.io/tkp.bio/public/demo3.gif">', // Update this path to your gif file
     content: 'This is an example video post.'
   },
   // ... more posts
