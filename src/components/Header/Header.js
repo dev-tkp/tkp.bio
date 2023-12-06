0cb3f9cd4caf7e1cf8904f8d9f83b32c96c91acd
dev-tkp <tkpark0504@gmail.com>
2023-12-06 17:40:26 +0900

    design polishing

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
 import './Header.css'; // Make sure you have the corresponding CSS file
+import upArrowIcon from './up-arrow.svg'; // Path to your up arrow icon
+import downArrowIcon from './down-arrow.svg'; // Path to your down arrow icon
+import copyIcon from './copy-icon.svg'; // Path to your copy icon
+
 
 const Header = () => {
   const [showEmail, setShowEmail] = useState(false);
-  const email = "your-email@example.com"; // Replace with your actual email
+  const email = "tkpark0504@gmail.com";
 
   const toggleEmail = () => {
     setShowEmail(!showEmail);
@@ -14,17 +18,14 @@ const Header = () => {
       <div className="header-content">
         <span className="name">박태강</span>
         <button className="email-toggle" onClick={toggleEmail}>
-          {showEmail ? "↑" : "↓"}
+          <img src={showEmail ? upArrowIcon : downArrowIcon} alt="Toggle Email" />
         </button>
       </div>
       {showEmail && (
         <div className="email-box">
           <span>{email}</span>
-          <button 
-            className="copy-button" 
-            onClick={() => navigator.clipboard.writeText(email)}
-          >
-            Copy
+          <button className="copy-button" onClick={() => navigator.clipboard.writeText(email)}>
+            <img src={copyIcon} alt="Copy Email" />
           </button>
         </div>
       )}
