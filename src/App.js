fdd24072e4937d6e201c03fd9b1c5d153b833d23
dev-tkp <tkpark0504@gmail.com>
2023-12-06 16:13:08 +0900

    heavy troubleshooting on image

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Header from './components/Header/Header';
 import VideoList from './components/VideoList/VideoList';
 import videoPosts from './videoPostsData';
 import './App.css';
 
 const App = () => {
-  const [containerHeight, setContainerHeight] = useState(window.innerHeight * 0.7);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setContainerHeight(window.innerHeight * 0.7);
-    };
-
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
+ 
   return (
     <div className="app">
-      <div className="gif-container">
+      <div className="content-container" >
         <Header />
-        <VideoList videoPosts={videoPosts} containerHeight={containerHeight} />
+        <div className="gif-container">
+          <VideoList videoPosts={videoPosts} />
+        </div>
       </div>
     </div>
   );
--- src/components/VideoPost/VideoPost.js
+++ src/components/VideoPost/VideoPost.js
@@ -3,9 +3,9 @@
 import React from 'react';
 import './VideoPost.css';
 
-const VideoPost = ({ videoSrc, content, style }) => {
+const VideoPost = ({ videoSrc, content }) => {
   return (
-    <div className="video-post" style={{ ...style, backgroundImage: `url(${videoSrc})` }}>
+    <div className="video-post" style={{ backgroundImage: `url(${videoSrc})` }}>
       <div className="content">
         {content}
       </div>
