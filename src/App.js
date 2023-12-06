1ebdb7784fbfed5d29772cbcc855103d69265a52
dev-tkp <tkpark0504@gmail.com>
2023-12-06 23:03:20 +0900

    polishing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import useViewportSize from './useViewportSize';
 import Header from './components/Header/Header';
 import VideoList from './components/VideoList/VideoList';
 import videoPosts from './videoPostsData';
 import './App.css';
 
 const App = () => {
- 
+  const { height, width } = useViewportSize();
+  const [containerHeight, setContainerHeight] = useState('100vh');
+
+  useEffect(() => {
+    // Set container height to 80% for desktop and 100% for mobile
+    if (width >= 769) {
+      setContainerHeight('80vh');
+    } else {
+      setContainerHeight(`${height}px`);
+    }
+  }, [height, width]);
+
   return (
     <div className="app">
-      <div className="content-container" >
+      <div className="content-container" style={{ height: containerHeight }}>
         <Header />
         <div className="gif-container">
           <VideoList videoPosts={videoPosts} />
